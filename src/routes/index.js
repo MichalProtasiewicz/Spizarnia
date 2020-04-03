39a6f2db9f2164beec1e23a02710d87afd13c863
Michał Protasiewicz <protez95@gmail.com>
2020-04-03 20:26:56 +0200

    commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,3 @@
 export const routes = {
   home: '/',
-  notes: '/notes',
-  note: '/notes/details/:id',
-  twitters: '/twitters',
-  twitter: '/twitters/details/:id',
-  articles: '/articles',
-  article: '/articles/details/:id',
-  login: '/login',
-  new: '/:pageType(twitters|notes|articles)/new',
 };
---
+++ src/views/MainPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MainPage = () => (
+  <>
+    <h1>Hello</h1>
+  </>
+);
+
+export default MainPage;
